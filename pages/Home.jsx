26e503419af3f4e4af8634d6fee0e333cccbbe58
Guillermo Guevara <guillermoguevara887@gmail.com>
2025-04-14 18:48:03 -0700

    Add updated avatar to Home section

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,9 @@
+import avatar from '../src/assets/avatar1.png';
+
 const Home = () => {
     return (
         <section id="home" className="home-section">
+            <img src={avatar} alt="Avatar" className="avatar" />
             <h1>Hi, I’m Guillermo</h1>
             <p>I build modern, responsive web interfaces using React.</p>
         </section>
