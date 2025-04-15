06254743735253634eb075a776d0af135561b7dd
Guillermo Guevara <guillermoguevara887@gmail.com>
2025-04-14 18:53:14 -0700

    Add AOS animations to Home section

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,11 +2,14 @@ import avatar from '../src/assets/avatar1.png';
 
 const Home = () => {
     return (
-        <section id="home" className="home-section">
-            <img src={avatar} alt="Avatar" className="avatar" />
-            <h1>Hi, I’m Guillermo</h1>
-            <p>I build modern, responsive web interfaces using React.</p>
+        <section id="home" className="home-section" data-aos="fade-up">
+            <img src={avatar} alt="Avatar" className="avatar" data-aos="zoom-in" />
+            <h1 data-aos="fade-up" data-aos-delay="200">Hi, I’m Guillermo</h1>
+            <p data-aos="fade-up" data-aos-delay="400">
+                I build modern, responsive web interfaces using React.
+            </p>
         </section>
+
     );
 };
 
