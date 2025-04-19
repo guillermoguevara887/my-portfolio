43e725df0c25e80339847ee031c7dbf3bb8a4fb9
Guillermo Guevara <guillermoguevara887@gmail.com>
2025-04-19 13:14:35 -0700

    Make navbar hamburger menu responsive with animation and close on click

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,34 @@
 // src/components/Navbar.jsx
 
 import React from 'react';
+import { useState } from 'react';
+
+
+
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((prev) => !prev)
+
+    }
     return (
         <nav className="navbar">
             <div className="navbar-logo">Guillermo Guevara</div>
-            <ul className="navbar-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+
+            <div className='hamburger' onClick={toggleMenu}>
+                ☰
+            </div>
+
+
+            <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+                <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+                <li><a href="#about" onClick={toggleMenu}>About</a></li>
+                <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+                <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
             </ul>
+
         </nav>
     );
 };
