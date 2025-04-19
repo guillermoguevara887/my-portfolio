73a9512d39811b3e005bb5ca552fa6ca1e095ded
Guillermo Guevara <guillermoguevara887@gmail.com>
2025-04-19 14:55:14 -0700

    Add download button for resume

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,7 +7,12 @@ const Home = () => {
             <h1 data-aos="fade-up" data-aos-delay="200">Hi, I’m Guillermo</h1>
             <p data-aos="fade-up" data-aos-delay="400">
                 I build modern, responsive web interfaces using React.
+
             </p>
+            <a href="/cv.pdf" download className="download-btn">
+                Download My Resume
+            </a>
+
         </section>
 
     );
